78954c0a3058ee416a60bb151b4f3f32544fc905
taehwee_park <hweetae@gmail.com>
2023-06-28 15:54:42 +0900

    [New Feature] TestRequestBodyDTO creation / GET Request with request body

--- demo/src/main/java/com/example/demo/controller/TestController.java
+++ demo/src/main/java/com/example/demo/controller/TestController.java
@@ -1,6 +1,12 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.ResponseDTO;
+import com.example.demo.dto.TestRequestBodyDTO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("test")
@@ -10,13 +16,21 @@ public class TestController {
         return "Hello World!";
     }
 
+    // path variable (localhost:8080/test/123)
     @GetMapping("/{id}")
     public String testControllerWithPathVariable(@PathVariable(required = false) int id) {
         return "Hello World with path variable" + id;
     }
 
+    // request param (localhost:8080/test/requestParam?param=123)
     @GetMapping("/requestParam")
     public String testControllerWithRequestParam(@RequestParam(required = false) int param) {
         return "Hello World with request parma " + param;
+    }
+
+    // using input DTO
+    @GetMapping("/requestBody")
+    public String testControllerWithRequestBody(@RequestBody TestRequestBodyDTO testRequestBodyDTO) {
+        return "Hello World with request body. ID: " + testRequestBodyDTO.getId() + " and Message: " + testRequestBodyDTO.getMessage();
     }
 }
