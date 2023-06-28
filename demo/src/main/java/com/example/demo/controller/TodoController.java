41df67546edd2bbc754d90fe691b730d8fcd9b31
taehwee_park <hweetae@gmail.com>
2023-06-28 17:17:52 +0900

    [New Feature] TodoService Creation in Service Layer, using this service from TodoController in Controller layer

--- demo/src/main/java/com/example/demo/controller/TodoController.java
+++ demo/src/main/java/com/example/demo/controller/TodoController.java
@@ -1,9 +1,30 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.ResponseDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.demo.service.TodoService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("todo")
 public class TodoController {
+    @Autowired TodoService service;
+
+    @GetMapping("/testService")
+    public ResponseEntity<?> testTodo() {
+        String str = service.testService();
+        List<String> list = new ArrayList<>();
+        list.add(str);
+
+        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+
+        return ResponseEntity.ok().body(response);
+    }
 }
